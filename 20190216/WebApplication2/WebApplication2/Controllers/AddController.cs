e6c1d134756225daa40bc8c2e3626886e701f7c5
Bill Ho <k90262xp@hotmail.com>
2019-02-17 16:00:58 +0800

    added practices code of 20190126 and 20190216

---
+++ 20190216/WebApplication2/WebApplication2/Controllers/AddController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    public class AddController : ApiController
+    {
+        public int Get(int no1, int no2)
+        {
+            return no1 + no2;
+        }
+    }
+}
---
+++ 20190216/WebApplication2/WebApplication2/Controllers/BMIController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    public class BMIController : ApiController
+    {
+        public double Get(double height, double weight)
+        {
+            double h = height / 100;
+            return Math.Round(weight / h / h, 2);
+        }
+
+    }
+}
