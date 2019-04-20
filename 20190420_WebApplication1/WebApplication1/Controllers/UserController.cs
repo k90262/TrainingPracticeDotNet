af520c099ea0510dec354535a3faf3cd2e1a256f
Bill Ho <k90262xp@hotmail.com>
2019-04-20 12:53:40 +0800

    update the content of 20190420: init an MVC project with adding 'C' and 'V' part (ASP.NET pre lession 1)

---
+++ 20190420_WebApplication1/WebApplication1/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public ActionResult Login(string username, string passwordj)
+        //public ActionResult Login(LoginModel obj)
+        {
+            return RedirectToAction("LoginResult");
+        }
+
+        public ActionResult LoginResult()
+        {
+            return View();
+        }
+    }
+}
--- 20190420_WebApplication1/WebApplication1/Properties/AssemblyInfo.cs
+++ 20190420_WebApplication1/WebApplication1/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // 下列 GUID 為專案公開 (Expose) 至 COM 時所要使用的 typelib ID
-[assembly: Guid("3a87d219-4784-4d70-9603-8c8b7a7bdca3")]
+[assembly: Guid("5d636d3d-1de5-419c-8bfb-695539f687e2")]
 
 // 組件的版本資訊是由下列四項值構成:
 //
