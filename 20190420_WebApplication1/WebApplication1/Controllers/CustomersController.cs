d54f3b770ca312baa57d29545ec8f777f35e708a
Bill Ho <k90262xp@hotmail.com>
2019-04-27 12:35:14 +0800

    created an db, a Entity Model, and controler&veiew for /Customers/Create (ASP.NET pre lession 2)

---
+++ 20190420_WebApplication1/WebApplication1/Controllers/CustomersController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Customer obj)
+        {
+            return View(obj);
+        }
+    }
+}
