8f85f0e35b9ddbadae0b40b693e2e9badbe1cbff
Bill Ho <k90262xp@hotmail.com>
2019-04-27 17:10:43 +0800

    finished query, added, delete functions simplely for /Customers/Index (ASP.NET pre lession 3)

--- 20190420_WebApplication1/WebApplication1/Controllers/CustomersController.cs
+++ 20190420_WebApplication1/WebApplication1/Controllers/CustomersController.cs
@@ -9,6 +9,15 @@ namespace WebApplication1.Controllers
 {
     public class CustomersController : Controller
     {
+        Database1Entities db = new Database1Entities();
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            //return View(db.Customers);
+            return View(db.Customers);
+        }
+
         // GET: Customers
         public ActionResult Create()
         {
@@ -18,7 +27,18 @@ namespace WebApplication1.Controllers
         [HttpPost]
         public ActionResult Create(Customer obj)
         {
-            return View(obj);
+            db.Customers.Add(obj);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Customer c = db.Customers.Find(id);
+            // 要自己判斷C存不存在
+            db.Customers.Remove(c);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
