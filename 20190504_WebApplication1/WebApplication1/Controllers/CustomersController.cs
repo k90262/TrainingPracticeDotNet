6ce500a96f09271734a5419d33a0f35921bea5f4
Bill Ho <k90262xp@hotmail.com>
2019-05-04 13:59:33 +0800

    Created new web API (Get, Post, Put) (ASP.NET pre lesson 4)

---
+++ 20190504_WebApplication1/WebApplication1/Controllers/CustomersController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class CustomersController : ApiController
+    {
+        Database1Entities db = new Database1Entities();
+
+        public IEnumerable<Customer> Get()
+        {
+            return db.Customers;
+        }
+
+        public Customer Get(int id)
+        {
+            Customer obj = db.Customers.Find(id);
+            if (obj != null)
+                return obj;
+            else
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+        }
+
+        public Customer Post(Customer obj)
+        {
+            db.Customers.Add(obj);
+            db.SaveChanges();
+            return obj;
+        }
+
+        public Customer Put(int id,Customer obj)
+        {
+            obj.CustomerId = id;
+            db.Entry(obj).State=EntityState.Modified;
+            db.SaveChanges();
+            return obj;
+
+        }
+
+    }
+}
