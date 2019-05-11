1935948ade7f27cdfb832af1f98f2b7366ff87ac
Bill Ho <k90262xp@hotmail.com>
2019-05-11 15:07:56 +0800

    introduce to view layout and view bag (ASP.NET lession 2)

---
+++ 20190511_WebApplication1/WebApplication1/Controllers/HealthController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HealthController : Controller
+    {
+        // GET: Health
+        public ActionResult BMI()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult BMI(double Height, double Weight)
+        {
+            double h = Height / 100;
+            double bmi = Weight / h / h;
+
+            ViewBag.Height = Height;
+            ViewBag.Weight = Weight;
+            ViewBag.BMI = bmi;
+            return View();
+        }
+    }
+}
