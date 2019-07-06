282d2f32eeaa3b13e48898f8df6884a658ce4583
Bill Ho <ycho.bill.lab@gmail.com>
2019-07-06 15:29:01 +0800

    init a console project in the same solution of web api project (ASP.NET Web service lession 2)

---
+++ 20190706/ConsoleApplication1/Program.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an key to call web api ...");
+            Console.ReadKey();
+
+            WebClient client = new WebClient();
+            client.BaseAddress = "http://localhost:1856";
+            string s = client.DownloadString("/api/Customers");
+            //Console.WriteLine(s);
+
+            //var result = (JArray)JsonConvert.DeserializeObject(s);
+            //Console.Write(result.Count.ToString());
+            var result = JsonConvert.DeserializeObject<Customer[]>(s);
+            foreach (Customer c in result)
+            {
+                Console.WriteLine("{0}\t{1}\t{2}", c.CustomerID, c.Name, c.Birthday);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    class Customer
+    {
+        public int CustomerID { get; set; }
+        public string Name { get; set; }
+        public DateTime? Birthday { get; set; }
+    }
+}
