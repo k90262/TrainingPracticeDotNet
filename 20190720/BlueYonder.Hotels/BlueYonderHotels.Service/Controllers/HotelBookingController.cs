7951ec64e6b3d11077e10e0c6522be90928ce201
Bill Ho <ycho.bill.lab@gmail.com>
2019-07-20 17:40:09 +0800

    finish Mod 04 - pipline/middleware/filter (ASP.NET Web service lession 6)

---
+++ 20190720/BlueYonder.Hotels/BlueYonderHotels.Service/Controllers/HotelBookingController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BlueYonder.Hotels.DAL.Models;
+using BlueYonder.Hotels.DAL.Repository;
+using BlueYonderHotels.Service.Attributes;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlueYonderHotels.Service.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HotelBookingController : ControllerBase
+    {
+        private readonly IHotelBookingRepository _hotelBookingRepository;
+
+        public HotelBookingController(IHotelBookingRepository hotelBookingRepository)
+        {
+            _hotelBookingRepository = hotelBookingRepository;
+        }
+        // test by POSTMAN (using availablility api (cached) compared with Reservation api (no-cached))
+        [HttpGet("Availability/{date}")]
+        [Cache("X-No-Cache")]
+        public IEnumerable<Room> GetAvailability(DateTime date)
+        {
+            return _hotelBookingRepository.GetAvaliabileByDate(date);
+        }
+
+        [HttpGet("Reservation")]
+        public IEnumerable<Reservation> GetReservation()
+        {
+            return _hotelBookingRepository.GetAllReservation();
+        }
+
+
+        [HttpDelete("Reservation/{reservationId}")]
+        public async Task DeleteReservation(int reservationId)
+        {
+            await _hotelBookingRepository.DeleteReservation(reservationId);
+        }
+
+    }
+}
