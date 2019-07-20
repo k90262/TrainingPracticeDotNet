a6515d911863b102b7af4fc376e74400cf301afd
Bill Ho <ycho.bill.lab@gmail.com>
2019-07-20 12:51:45 +0800

    init for Lab2 env (ASP.NET Web service lession 5)

--- 20190720/DAL/Models/Booking.cs
+++ 20190720/DAL/Models/Booking.cs
@@ -1,17 +1,16 @@
  using System;
 
- namespace DAL.Models
- {
-     public class Booking
-     {
-         public int BookingId { get; set; }
-         public Room Room { get; set; }
-         public DateTime DateCreated { get; set; }
-         public DateTime CheckIn { get; set; }
-         public DateTime CheckOut { get; set; }
-         public int Guests { get; set; }
-         public decimal TotalFee { get; set; }
-         public bool Paid { get; set; }
-         public Traveler Traveler { get; set; }
-     }
- }
+    namespace DAL.Models
+    {
+        public class Booking
+        {
+            public int BookingId { get; set; }
+            public Room Room { get; set; }
+            public DateTime DateCreated { get; set; }
+            public DateTime CheckIn { get; set; }
+            public DateTime CheckOut { get; set; }
+            public int Guests { get; set; }
+            public bool Paid { get; set; }
+            public Traveler Traveler { get; set; }
+        }
+    }
