4c4487b73b3bbe094a177c476ae3119dc10943f1
Bill Ho <ycho.bill.lab@gmail.com>
2019-09-28 12:01:10 +0800

    [DemoApp] tranditional way (non-Polymorphism)

--- 20190928_DemoApp/DemoApp/Form1.Designer.cs
+++ 20190928_DemoApp/DemoApp/Form1.Designer.cs
@@ -28,12 +28,86 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnMove = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnMove
+            // 
+            this.btnMove.Location = new System.Drawing.Point(30, 36);
+            this.btnMove.Name = "btnMove";
+            this.btnMove.Size = new System.Drawing.Size(75, 23);
+            this.btnMove.TabIndex = 0;
+            this.btnMove.Text = "移動";
+            this.btnMove.UseVisualStyleBackColor = true;
+            this.btnMove.Click += new System.EventHandler(this.btnMove_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 90);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(28, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(30, 176);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(28, 215);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnMove);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnMove;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
     }
 }
 
--- 20190928_DemoApp/DemoApp/Form1.cs
+++ 20190928_DemoApp/DemoApp/Form1.cs
@@ -12,9 +12,63 @@ namespace DemoApp
 {
     public partial class Form1 : Form
     {
+        private int no;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnMove_Click(object sender, EventArgs e)
+        {
+            //if (no == 1)
+            //    button1.Left += 10;
+            //else if (no == 2)
+            //    button2.Left += 10;
+            //else if (no == 3)
+            //    button3.Left += 10;
+            //else if (no == 4)
+            //    button4.Left += 10;
+            //else
+            //    MessageBox.Show("Please choose a button!");
+
+            switch (no)
+            {
+                case 1:
+                    button1.Left += 10;
+                    break;
+                case 2:
+                    button2.Left += 10;
+                    break;
+                case 3:
+                    button3.Left += 10;
+                    break;
+                case 4:
+                    button4.Left += 10;
+                    break;
+                default:
+                    MessageBox.Show("Please choose a button!");
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            no = 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            no = 2;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            no = 3;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            no = 4;
         }
     }
 }
