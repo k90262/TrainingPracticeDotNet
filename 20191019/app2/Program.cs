dfaf5ed5256103dd788839bf6cee3e664313a379
Bill Ho <ycho.bill.lab@gmail.com>
2019-10-19 13:51:31 +0800

    [App2][App3] (1. change the num of parameter by using params, (2. init App3

--- 20191019/app2/Program.cs
+++ 20191019/app2/Program.cs
@@ -4,15 +4,16 @@ namespace app2
 {
     class Program
     {
-        static int Sum(int a, int b, int c)
+        static int Sum(params int[] data)
         {
-            int total;
-            total = a + b + c;
+            int total = 0;
+            foreach (int no in data)
+                total += no;
             return total;
         }
         static void Main(string[] args)
         {
-            int result = Sum(10, 20, 30);
+            int result = Sum(10, 20, 30, 40, 50, 1, 2, 3, 4, 5);
             Console.WriteLine(result);
         }
     }
---
+++ 20191019/app3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace app3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int[] data = new int[10];
+            // int[] data = new int[4] {10, 20, 30 ,40};
+            // int[] data = new int[] {10, 20, 30 ,40};
+            int[] data = {1, 2, 3 ,4, 5, 6, 7, 8, 9, 10};
+            foreach(int no in data)
+            {
+                Console.WriteLine(no);
+            }
+        }
+    }
+}
