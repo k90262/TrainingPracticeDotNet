da03353c7085cbcf5a0dda3c1b41986c1226ddd4
Bill Ho <ycho.bill.lab@gmail.com>
2019-10-19 12:14:13 +0800

    [App1][App2] 1. introduce to parameter type (call by value/reference). 2. init App2

--- 20191019/app1/Program.cs
+++ 20191019/app1/Program.cs
@@ -4,17 +4,27 @@ namespace app1
 {
     class Program
     {
-        static int Sum(int a, int b, int c)
+        static int Sum(int a, ref int b, out int c, ref int[] args) // in, in/out, out
         {
+            a = 1;
+            b = 2;
+            c = 3;
+            args[0] = args[1] + args[2];
             int total;
             total = a+b+c;
             return total;
         }
         static void Main(string[] args)
         {
+            int x = 10;
+            int y = 20;
+            int z;
+            int[] data = {1, 2, 3};
             int result;
-            result = Sum(10, 20, 30);
+            result = Sum(x, ref y, out z, ref data); // ref type: 可加ref可不加
             Console.WriteLine(result);
+            Console.WriteLine($"x={x}, y={y}, z={z}");
+            Console.WriteLine($"data[0]={data[0]}");
         }
     }
 }
---
+++ 20191019/app2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace app2
+{
+    class Program
+    {
+        static int Sum(int a, int b, int c)
+        {
+            int total;
+            total = a + b + c;
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            int result = Sum(10, 20, 30);
+            Console.WriteLine(result);
+        }
+    }
+}
