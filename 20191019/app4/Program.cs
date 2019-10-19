d9a80a2dfbbbe9aa721b1f4afaffd28423662d7e
Bill Ho <ycho.bill.lab@gmail.com>
2019-10-19 16:38:06 +0800

    [App4] init a sync function

---
+++ 20191019/app4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace app4
+{
+    class Program
+    {
+        static int M1 (int no)
+        {
+            int result = no * 2;
+            Console.WriteLine(result);
+            return result;
+        }
+        static int M2 (int no)
+        {
+            int result = no * 4;
+            Console.WriteLine(result);
+            return result;
+        }
+        static int M3 (int no)
+        {
+            int result = no * 8;
+            Console.WriteLine(result);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            int no = 5;
+            int no1 = M1(no);
+            Console.WriteLine("M1 OK!");
+                        
+            int no2 = M2(no);
+            Console.WriteLine("M2 OK!");
+            
+            int no3 = M3(no);
+            Console.WriteLine("M3 OK!");
+        }
+    }
+}
