f16108a70aaf813230d7546022c2e8c121044eb3
Bill Ho <ycho.bill.lab@gmail.com>
2019-10-19 16:40:18 +0800

    [App4] demo async by Task

--- 20191019/app4/Program.cs
+++ 20191019/app4/Program.cs
@@ -1,22 +1,23 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace app4
 {
     class Program
     {
-        static int M1 (int no)
+        static int M1(int no)
         {
             int result = no * 2;
             Console.WriteLine(result);
             return result;
         }
-        static int M2 (int no)
+        static int M2(int no)
         {
             int result = no * 4;
             Console.WriteLine(result);
             return result;
         }
-        static int M3 (int no)
+        static int M3(int no)
         {
             int result = no * 8;
             Console.WriteLine(result);
@@ -25,14 +26,40 @@ namespace app4
         static void Main(string[] args)
         {
             int no = 5;
-            int no1 = M1(no);
-            Console.WriteLine("M1 OK!");
-                        
-            int no2 = M2(no);
-            Console.WriteLine("M2 OK!");
-            
-            int no3 = M3(no);
-            Console.WriteLine("M3 OK!");
+            //int no1 = M1(no);  // sync
+            // Func<int, int> f1 = M1;
+            // int no1 = f1.Invoke(no); // sync
+            Task t1 = Task.Run(() =>
+            {    // asyc
+                int no1 = M1(no);
+
+                Task t2 = Task.Run(() =>
+                {    // asyc
+                    int no2 = M2(no1);
+
+                    Task t3 = Task.Run(() =>
+                    {    // asyc
+                        int no3 = M3(no2);
+                        Console.WriteLine("M3 OK!");
+                    });
+
+                    Console.WriteLine("M2 OK!");
+                });
+
+                Console.WriteLine("M1 OK!");
+            });
+
+            //int no2 = M2(no);  // sync
+            //Func<int, int> f2 = M2;
+            //int no2 = f2.Invoke(no);  // sync            
+
+
+            //int no3 = M3(no);  // sync
+            //Func<int, int> f3 = M3;
+            //int no3 = f3(no); // it's also ok
+            //int no3 = f3.Invoke(no);  // sync
+
+            Console.ReadKey();
         }
     }
 }
