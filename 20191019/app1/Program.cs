4e815d883f2f4e0a2ef76318268b3bde41b7fa2b
Bill Ho <ycho.bill.lab@gmail.com>
2019-10-19 11:53:38 +0800

    [Console1][App1] init a console vscode project, introduct to task.json, launch.json

---
+++ 20191019/app1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace app1
+{
+    class Program
+    {
+        static int Sum(int a, int b, int c)
+        {
+            int total;
+            total = a+b+c;
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            int result;
+            result = Sum(10, 20, 30);
+            Console.WriteLine(result);
+        }
+    }
+}
