a29eb1d22b70229928ca1226107e85ef7eec8822
Bill Ho <ycho.bill.lab@gmail.com>
2019-10-26 12:17:17 +0800

    [app7] init for introduction to 擴充方法

---
+++ 20191026/app7/Class1.cs
@@ -0,0 +1,9 @@
+using System;
+
+public static class Class1
+{
+    public static void Show(this int no)
+    {
+       Console.WriteLine(no);
+    }
+}
---
+++ 20191026/app7/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace app7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int no1 = 10;
+            no1.Show();
+        }
+    }
+}
