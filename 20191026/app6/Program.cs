9ce35b2184ae8a603a1c325adace0f02b3ca936b
Bill Ho <ycho.bill.lab@gmail.com>
2019-10-26 11:30:47 +0800

    [app6] init before demo LINQ

---
+++ 20191026/app6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace app6
+{
+    class Program
+    {
+        static void Show(int[] obj)
+        {
+            foreach(int no in obj)
+            {
+                Console.Write($"{no}, ");
+            }
+            Console.WriteLine();
+        }
+        static void Main(string[] args)
+        {
+            int[] data = {21, 35, 12, 8, 6, 99, 19};
+            Show(data);
+        }
+    }
+}
