bb7aa155051562e2a82a54c40a269b5f87292436
Bill Ho <ycho.bill.lab@gmail.com>
2019-10-26 10:50:10 +0800

    [app5] task demo

---
+++ 20191026/app5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace app5
+{
+    class Program
+    {
+        static Task<int> Add(int a, int b) 
+        {
+            return Task<int>.Run(()=>{
+                Thread.Sleep(5000);
+                return a+b;
+            });
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting...");
+            Task<int> t = Add(10, 20);
+            t.Wait();
+            Console.WriteLine($"answer={t.Result}");
+        }
+    }
+}
