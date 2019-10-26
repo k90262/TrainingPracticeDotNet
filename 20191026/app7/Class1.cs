02c6d31a1e39953ca142e3e322ab3e840496012e
Bill Ho <ycho.bill.lab@gmail.com>
2019-10-26 12:25:25 +0800

    [app7] 擴充方法 demo

--- 20191026/app7/Class1.cs
+++ 20191026/app7/Class1.cs
@@ -1,9 +1,19 @@
 using System;
 
-public static class Class1
+public static class Class1 // 不一定要寫在同一個class內，只要是靜態類別即可
 {
     public static void Show(this int no)
     {
        Console.WriteLine(no);
+    }
+
+    public static int Double(this int no)
+    {
+        return no * 2;
+    }
+
+    public static int Mul(this int no, int refactor) // 帶參數
+    {
+        return no * refactor;
     }
 }
--- 20191026/app7/Program.cs
+++ 20191026/app7/Program.cs
@@ -8,6 +8,12 @@ namespace app7
         {
             int no1 = 10;
             no1.Show();
+
+            int no2 = no1.Double();
+            no2.Show();
+
+            int no3 = no2.Mul(10); // 步驟: 1. 於一個類別內定義Mul(), 2. 將Mul改成靜態(不用new即可被呼叫)), 3. 將Mul的第一個參數使用"this int xxx"，擴充int；並加入參數, 4. 將該類別設定成靜態類別. Done
+            no3.Show();
         }
     }
 }
