a52a3e4fb07a85f6d83d7e6ec426ec1a4267efac
Bill Ho <ycho.bill.lab@gmail.com>
2019-10-26 16:08:43 +0800

    [app10][app11] write and init for read

--- 20191026/app10/Program.cs
+++ 20191026/app10/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Text;
 
 namespace app10
 {
@@ -8,8 +9,9 @@ namespace app10
         static void Main(string[] args)
         {
             string s = "BBC中文網是面向全球華人並每日更新的新聞資訊網站，提供客觀及時和凖確的內容以及對新聞故事的深入分析和評論。我們也提供繁體版臉書。";
+            byte[] buffer = Encoding.Default.GetBytes(s);
             FileStream fs = new FileStream(@"C:\Demos\test.txt", FileMode.Create);
-            
+            fs.Write(buffer, 0, buffer.Length);
             fs.Close();
             Console.WriteLine("OK");
         }
---
+++ 20191026/app11/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace app11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(@"C:\Demos\test.txt", FileMode.Open);
+
+            fs.Close();
+            Console.WriteLine("OK");
+        }
+    }
+}
