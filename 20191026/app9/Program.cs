dbe3f27d0c6de26f8b8ba9faaf44193eeb66e2dd
Bill Ho <ycho.bill.lab@gmail.com>
2019-10-26 15:12:23 +0800

    [app9] used Process and LINQ to list the first 5 program used the higher memory

--- 20191026/app9/Program.cs
+++ 20191026/app9/Program.cs
@@ -9,9 +9,12 @@ namespace app9
         static void Main(string[] args)
         {
             var result = from p in Process.GetProcesses() 
+            orderby p.WorkingSet64 descending
             select p; 
+            //var result2 = result.Take(5) // 取前五個
+            //                .Skip(3); // 省略前三個
 
-            foreach(var p in result)
+            foreach(var p in result.Take(5))
             {
                 Console.WriteLine($"{p.ProcessName}.exe\t{p.WorkingSet64}");
             }
