4434bf50f5d56e7e03fae6f7a4474102c270a5cf
Bill Ho <ycho.bill.lab@gmail.com>
2019-10-26 16:27:45 +0800

    [app12] copy file by filestream

---
+++ 20191026/app12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace app12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Read and Coy to a new file
+            FileStream fs = new FileStream(@"C:\Demos\test.txt", FileMode.Open);
+            byte[] buffer = new byte[fs.Length];
+            fs.Read(buffer, 0, buffer.Length);
+            fs.Close();
+
+            FileStream fs2 = new FileStream(@"C:\Demos\test2.txt", FileMode.Create);
+            fs2.Write(buffer, 0, buffer.Length);
+            fs2.Close();
+            Console.WriteLine("OK");
+        }
+    }
+}
