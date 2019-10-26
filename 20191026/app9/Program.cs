3b4a785b2a1107d53016132d3d42824a40c6d2e8
Bill Ho <ycho.bill.lab@gmail.com>
2019-10-26 14:55:41 +0800

    [app9] init for Process+LINQ

---
+++ 20191026/app9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using System.Diagnostics;
+using System;
+
+namespace app9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = from p in Process.GetProcesses() 
+            select p; 
+
+            foreach(var p in result)
+            {
+                Console.WriteLine($"{p.ProcessName}.exe\t{p.WorkingSet64}");
+            }
+
+
+        }
+    }
+}
