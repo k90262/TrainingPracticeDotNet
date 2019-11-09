e0e53464df20236f27b39ae0d5d164a7edb377f7
Bill Ho <ycho.bill.lab@gmail.com>
2019-11-09 10:40:33 +0800

    [web1] demo to run and access a static file
    by link http://localhost:5000/demo01.htm after 'dotnet run'

--- 20191109_webapi/web1/Startup.cs
+++ 20191109_webapi/web1/Startup.cs
@@ -40,8 +40,8 @@ namespace web1
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            //app.UseHttpsRedirection();
             app.UseMvc();
         }
     }
