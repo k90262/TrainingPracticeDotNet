e26996b2961aa610799a58461f3b3ff2196a3172
Bill Ho <ycho.bill.lab@gmail.com>
2019-12-07 12:38:33 +0800

    [web][demo07] (1. init move() function of Class Ball (.ts->.js), (2. init Canvas element (.htm)

--- 20191109_webapi/web1/wwwroot/Ball.js
+++ 20191109_webapi/web1/wwwroot/Ball.js
@@ -1,11 +1,33 @@
+// pre-installation:
+// 1. nodejs (npm), 2. npm install -g typescript
+//
+// reference site:
+// typescript.org
+// typescriptlang.org
+//
+// compile command (.ts -> .js):
+// tsc .\wwwroot\Ball.ts
 var Ball = /** @class */ (function () {
-    function Ball(x, y, r, c) {
+    function Ball(x, y, r, c, dx, dy) {
         this.x = x;
         this.y = y;
         this.r = r;
         this.c = c;
+        this.dx = dx;
+        this.dy = dy;
     }
+    Ball.prototype.move = function () {
+        this.x += this.dx;
+        this.y += this.dy;
+    };
+    Ball.prototype.show = function () {
+        console.log("(" + this.x + ", " + this.y + ")");
+    };
     return Ball;
 }());
-var obj = new Ball(300, 200, 50, "blue");
+var obj = new Ball(300, 200, 50, "blue", 5, 10);
+for (var i = 0; i < 10; i++) {
+    obj.move();
+    obj.show();
+}
 alert(obj.c);
--- 20191109_webapi/web1/wwwroot/Ball.ts
+++ 20191109_webapi/web1/wwwroot/Ball.ts
@@ -1,9 +1,32 @@
+// pre-installation:
+// 1. nodejs (npm), 2. npm install -g typescript
+//
+// reference site:
+// typescript.org
+// typescriptlang.org
+//
+// compile command (.ts -> .js):
+// tsc .\wwwroot\Ball.ts
+
 class Ball {
 
-    constructor(public x: number,public  y: number,public  r: number,public  c: string){
+    constructor(public x: number,public  y: number,public  r: number,public  c: string, public dx: number, public dy: number){
 
+    }
+
+    move() {
+        this.x += this.dx;
+        this.y += this.dy;
+    }
+
+    show() {
+        console.log(`(${this.x}, ${this.y})`);
     }
 }
 
-let obj = new Ball(300, 200, 50, "blue");
+let obj = new Ball(300, 200, 50, "blue", 5, 10);
+for(var i=0; i<10; i++) {
+    obj.move();
+    obj.show();
+}
 alert(obj.c);
